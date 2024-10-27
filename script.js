5e9175b120bed47346add06511f50fc158824b89
FDX-UI <142981582+FDX-UI@users.noreply.github.com>
2024-10-27 16:09:08 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-document.getElementById('a1').addEventListener('click', function() {
-    document.body.classList.toggle('a2');
-    // 114514
-    const button = document.getElementById('a1');
-    if (document.body.classList.contains('a2')) {
-        button.textContent = '切换到白天模式';
-    } else {
-        button.textContent = '切换到夜晚模式';
-    }
+document.getElementById('toggleNightMode').addEventListener('click', function() {
+    document.body.classList.toggle('night-mode');
 });
