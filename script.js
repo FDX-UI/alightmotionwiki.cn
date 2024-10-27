13e070c9a1151f2465de55dced218cc23b477e12
FDX-UI <142981582+FDX-UI@users.noreply.github.com>
2024-10-27 13:13:09 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('a1').addEventListener('click', function() {
+    document.body.classList.toggle('a2');
+    
+    // 更新按钮文本
+    const button = document.getElementById('a1');
+    if (document.body.classList.contains('a2')) {
+        button.textContent = '切换到白天模式';
+    } else {
+        button.textContent = '切换到夜晚模式';
+    }
+});
